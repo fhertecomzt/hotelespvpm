23106a2c6200c621a9cbaacf361138ff129442b9
fhertecomzt <fhertecorl2@gmail.com>
2026-08-16 21:43:42 -0700

    Update icono de aps e instalación de cloudfare turnstile

--- swaos-recepcion/src/main.jsx
+++ swaos-recepcion/src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+// 1. Importamos el motor PWA
+import { registerSW } from "virtual:pwa-register";
 
+// 2. Activamos la instalación en segundo plano
+registerSW({ immediate: true });
+
+// 3. Renderizamos la aplicación una sola vez
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
--- swaos-recepcion/vite.config.js
+++ swaos-recepcion/vite.config.js
@@ -1,15 +1,51 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    //  Pplugin PWA aquí, dentro de los corchetes de plugins
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "SWAOS Plataforma Operativa",
+        short_name: "SWAOS",
+        description:
+          "Sistema inteligente para gestión de limpieza y mantenimiento.",
+        theme_color: "#0f172a",
+        background_color: "#f8fafc",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      },
+    }),
+  ],
+  // configuración del proxy local
   server: {
     proxy: {
-      // Le decimos a Vite que intercepte estas rutas relativas
       "/sistema/swaos-api": {
-        target: "http://localhost/hotelespvpm/", // La dirección de tu servidor Apache/XAMPP local
-        changeOrigin: true, // Necesario para evitar problemas de CORS en local
-        secure: false, // Como estamos en localhost sin https, lo dejamos en false
+        target: "http://localhost/hotelespvpm/",
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
